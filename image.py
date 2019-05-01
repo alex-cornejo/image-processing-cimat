339054c453e6ab768d7b628fc3afa62b4cf805a7
Myrna <myrnacastillo2@gmail.com>
2019-04-30 21:55:24 -0500

    normalizer added

--- image.py
+++ image.py
@@ -2,8 +2,9 @@ from __future__ import division   # impone aritmética no entera en la división
 from PIL import Image             # funciones para cargar y manipular imágenes
 import numpy as np                # funciones numéricas (arrays, matrices, etc.)
 import matplotlib.pyplot as plt   # funciones para representación gráfica
+from main import normalize
 
-I = Image.open("lena2.png") #Cargar la imagen
+I = Image.open("lena.png") #Cargar la imagen
 
 plt.imshow(np.asarray(I))
 #plt.show()                             #Ver imagen con Python
@@ -13,12 +14,13 @@ print (I.size, I.mode, I.format)        # Obtener tamaño, tipo (escala de grise
 I1 = I.convert('L')                     # Convierte a escala de grises 
 #I1.show()
 print (I1.size, I1.mode, I1.format)
-plt.imshow(np.asarray(I1), cmap='gray')
+plt.imshow(np.asarray(I1))
 #plt.show() 
 
 a = np.asarray(I1,dtype=np.float32)
+a = normalize(a, 200, 250)
 Image.fromarray(a.astype(np.uint8)).save("lena2.png")
 print(a)
 
-plt.imshow(a,cmap='gray')
+plt.imshow(a)
 plt.show()
