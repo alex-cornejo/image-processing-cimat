00a884debca1adf51ea614a18dcfef96f8f7405f
Myrna <myrnacastillo2@gmail.com>
2019-04-30 21:14:15 -0500

    avance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy
+import matplotlib.pyplot as plt
 
 def normalize(image, a, b):
 	min = numpy.min(image)
@@ -10,10 +11,26 @@ def normalize(image, a, b):
 
 	return image
 
+def histograma(image):
+	vec=numpy.zeros(255)
+	for i in range(image.shape[0]):
+		for j in range(image.shape[1]):
+			aux=image[i][j]
+			vec[aux]+=1
+	return vec
 
-image = numpy.array([[1.0, 2.0, 3.0],[4.0, 5.0, 6.0]])
 
-print min
-print max
+def obtAcumulativo(vec, max):
+	vecresult=numpy.zeros(255)
 
-print (normalize(image, 0, 255))	
+	for i in range(max+1):
+		if(i==0):
+			vecresult[i]=vec[0]
+		else:
+			vecresult[i]=vec[i]+vecresult[i-1]
+	return vecresult
+
+
+image = numpy.array([[2, 2, 3],[4, 5, 6]])
+x=histograma(image)
+print (obtAcumulativo(x, 6))	
